d0b46736d59bf95182c6da85f55fc090638272df
andresfr <andres.fernandez.rosa@gmail.com>
2013-02-06 19:24:17 +0100

    Añado intents a los botones en Activities

--- 2ndEdition/andres.fernandez.rosa/Main/src/com/example/lanzamientos/Main.java
+++ 2ndEdition/andres.fernandez.rosa/Main/src/com/example/lanzamientos/Main.java
@@ -14,22 +14,45 @@ public class Main extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.main);
 		Button boton1 = (Button) this.findViewById(R.id.boton1);
 		boton1.setOnClickListener(new OnClickListener() {
+
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent iUrl= new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.es"));
-				activity
+				Intent iUrl = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.es"));
+				startActivity(iUrl);
+			}
+		});
+
+		Button boton2 = (Button) this.findViewById(R.id.boton2);
+		boton2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent iViewPhone = new Intent(Intent.ACTION_VIEW);
+				iViewPhone.setData(Uri.parse("tel:677-371-074"));
+				startActivity(iViewPhone);
 			}
 		});
 		
-		
-	
-	
+		Button boton3 = (Button) this.findViewById(R.id.boton3);
+		boton3.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent iCallPhone = new Intent(Intent.ACTION_CALL);
+				iCallPhone.setData(Uri.parse("tel:677-371-074"));
+				startActivity(iCallPhone);
+			}
+		});
+			
+			
 	}
 
 	@Override
