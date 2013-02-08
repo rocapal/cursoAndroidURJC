72944727140bdcde7f32fbf115b1b1637cc0694d
Omar Pedraza <opedraza@ag.com>
2013-02-08 21:00:40 +0100

    Omar Pedraza's day 3 code

---
+++ 2ndEdition/omar.pedraza/02 ActivityTest/src/com/AGi/ActivityTest/Constants.java
@@ -0,0 +1,19 @@
+//
+//  Constants.java
+//  ActivityTest
+//
+//  Created by Omar Pedraza on 2/8/13.
+//  Copyright (c) 2013 American Greetings Interactive. All rights reserved.
+//
+package com.AGi.ActivityTest;
+
+public class Constants {
+    public static final String kMessage = "message";
+    public static final String kName = "name";
+    public static final String kNumber = "number";
+    public static final String kValue = "value";
+
+    public static final Integer kActivity1Request = 1;
+    public static final Integer kActivity2Request = 2;
+    public static final Integer kActivity3Request = 3;
+}
