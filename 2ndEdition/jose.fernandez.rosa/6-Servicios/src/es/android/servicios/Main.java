a497b8bdfe43e24a62d459c796ff3732ec5a8701
bartbender <bartbender@gmail.com>
2013-02-23 19:15:49 +0100

    Actualizando 6-Servicios

--- 2ndEdition/jose.fernandez.rosa/6-Servicios/src/es/android/servicios/Main.java
+++ 2ndEdition/jose.fernandez.rosa/6-Servicios/src/es/android/servicios/Main.java
@@ -1,6 +1,7 @@
 package es.android.servicios;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,23 +9,27 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class Main extends Activity {
-
-	
+public class Main extends Activity  {
+	private final String TAG = getClass().getSimpleName();
+	public static Context context;
+	private  TextView mTexto;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		context=this;		
+		mTexto= (TextView) findViewById(R.id.texto);
+		
 		Button btnStart   = (Button) findViewById(R.id.btn_start);
 		btnStart.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				Log.d("SERVICIO START","INICIANDO");
-				startService(new Intent(getApplicationContext(),MyService.class));	
+				initService();
 			}
 		});
 		
@@ -33,14 +38,35 @@ public class Main extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				stopService(new Intent(getApplicationContext(),MyService.class));
+				cancelService();
 			}
 		});
 		
+		
 	}
 
-	private void  InitService() {
+	private void  initService() {
+		Log.d(TAG,"Iniciando el servicio");
 		
+		MyService.regIServiceListener(new IServiceListener() {			
+			@Override
+			public void updateMessage(String cadena) {
+				Main.this.setTexto(cadena);
+				
+			}
+		});
+		
+		startService(new Intent(getApplicationContext(),MyService.class));	
+	}
+	
+	private void cancelService(){
+		Log.d(TAG,"Parando el servicio");
+		stopService(new Intent(getApplicationContext(),MyService.class));	
+	}
+	
+	public void setTexto(String cadena){
+		if (mTexto!=null) 
+			mTexto.setText(cadena);
 	}
 	
 	@Override
@@ -49,6 +75,7 @@ public class Main extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+
 	
 	
 	
--- 2ndEdition/jose.fernandez.rosa/6-Servicios/src/es/android/servicios/MyService.java
+++ 2ndEdition/jose.fernandez.rosa/6-Servicios/src/es/android/servicios/MyService.java
@@ -1,32 +1,57 @@
 package es.android.servicios;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MyService extends Service implements Runnable {
+	private final String TAG = getClass().getSimpleName();
 	private Handler mLoopHandler=new Handler();
+	private static ArrayList<IServiceListener> mSLs = new ArrayList<IServiceListener>();
+	private boolean mRunning=false;
 	@Override
 	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	
 	@Override
 	public void onCreate() {
-		// TODO Auto-generated method stub
-		super.onCreate();
-		
-		mLoopHandler.postDelayed(this, 3000);
-		
+		super.onCreate();		
+		mRunning = true;
+		Log.d(TAG, "Init service");
+		Toast.makeText(Main.context, "Init Service", Toast.LENGTH_SHORT).show();
+		mLoopHandler.postDelayed(this, 3000);		
+	}
+	
+	public static void regIServiceListener( IServiceListener sl){
+		mSLs.add(sl);
 	}
 
 	@Override
 	public void run() {
+		if (!mRunning) return;
 		
+		String today = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+		for (IServiceListener sl:mSLs) sl.updateMessage(today);
+		if (mLoopHandler!=null) mLoopHandler.postDelayed(this, 3000);
 		
 	}
 	
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		mRunning=false;
+		Log.d(TAG, "Stop service");
+		Toast.makeText(Main.context, "Stop Service", Toast.LENGTH_SHORT).show();
+	}
+	
+
 	
 }
