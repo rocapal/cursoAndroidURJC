dd9a8f7848b898a2724e279b17b5a26932ce6935
Óscar Álvarez Hidalgo <o.alvarez@ag.com>
2013-02-24 19:34:22 +0100

    Progress par example

--- 2ndEdition/oscar.alvarez/TimerTaskDia5/src/com/example/TimerTaskDia5/MyAsyncTask.java
+++ 2ndEdition/oscar.alvarez/TimerTaskDia5/src/com/example/TimerTaskDia5/MyAsyncTask.java
@@ -3,6 +3,7 @@ package com.example.TimerTaskDia5;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,7 +14,8 @@ import android.os.AsyncTask;
  */
 public class MyAsyncTask extends AsyncTask {
     Context mContext;
-    ProgressDialog pd;
+    ProgressDialog pd = null;
+    int progress;
 
     public MyAsyncTask(Context context) {
         mContext = context;
@@ -21,10 +23,15 @@ public class MyAsyncTask extends AsyncTask {
 
     @Override
     protected Object doInBackground(Object... objects) {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 100; i++)
+        {
+            try {
+                progress = i;
+                publishProgress(i);
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         return null;  //To change body of implemented methods use File | Settings | File Templates.
 
@@ -34,6 +41,9 @@ public class MyAsyncTask extends AsyncTask {
     protected void onPreExecute() {
         super.onPreExecute();    //To change body of overridden methods use File | Settings | File Templates.
         pd = new ProgressDialog(mContext);
+        pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        pd.setTitle("AsyncTask Background");
+        pd.setMessage("Executing...");
         pd.show();
     }
 
@@ -41,5 +51,13 @@ public class MyAsyncTask extends AsyncTask {
     protected void onPostExecute(Object o) {
         super.onPostExecute(o);    //To change body of overridden methods use File | Settings | File Templates.
         pd.dismiss();
+        Toast.makeText(mContext, "Progress bar successfully Presented", Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    protected void onProgressUpdate(Object... values) {
+        super.onProgressUpdate(values);    //To change body of overridden methods use File | Settings | File Templates.
+        pd.setProgress(progress);
+    }
+
 }
